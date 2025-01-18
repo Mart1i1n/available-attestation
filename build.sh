239390ee2ed7db824b58e7f4d9dfcd6dd6db0cb8
luxq <xueqian1991@gmail.com>
2025-01-18 10:33:47 +0800

    update build

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export VAL_NUM=${VAL_NUM}
+sed -i "s#TotalValidatorCount = 256#TotalValidatorCount = $VAL_NUM#g" ./modified_prysm/beacon-chain/forkchoice/doubly-linked-tree/allchain.go
 docker build -t attacker:latest -f dockerfiles/attack.Dockerfile .
 docker build -t geth:latest -f dockerfiles/geth.Dockerfile .
 docker build -t strategy:latest -f dockerfiles/strategy.Dockerfile .
